8b54d1ddc2847228143c3d76180d28f47caa2607
Diego Araujo <dieggo_taylor@hotmail.com>
2026-06-09 13:40:16 -0300

    Integração Server / GET

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import express, {json, Request, Response} from "express"
+
+const app = express()
+const port = process.env.PORT;
+
+app.use(json())
+
+app.get("/", (req:Request, res: Response)=>{
+    res.status(200).json({ player: "Beckhan"});
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running at port http://localhost:${port}`);
+})
