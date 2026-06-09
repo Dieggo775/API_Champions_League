ac48a159dcb3ee2abca65ca31bb3e478faf11306
Diego Araujo <dieggo_taylor@hotmail.com>
2026-06-09 14:57:04 -0300

    Implementação pasta util

---
+++ src/services/players-services.ts
@@ -0,0 +1,15 @@
+import { noContent, ok } from "../utils/http-helper";
+
+export const getPlayerService = async () => {
+    const data = { player: "ronaldo" };
+
+    let response = null
+
+    if(data){
+        response = await ok(data);
+    }else{
+        response = await noContent();
+    }
+
+    return response;
+};
